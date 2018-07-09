225b48e623a7f40d3e06fb7ea1b3c30e9facaf9d
Juan Jesus Balceda Pomachagua <juanbalceda@gmail.com>
2018-07-09 02:21:46 +0200

    SpringBoot first commit - Hibernate OK

---
+++ src/main/java/com/balceda/apps/platziteachers/PlatziTeachersApplication.java
@@ -0,0 +1,12 @@
+package com.balceda.apps.platziteachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziTeachersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziTeachersApplication.class, args);
+	}
+}
